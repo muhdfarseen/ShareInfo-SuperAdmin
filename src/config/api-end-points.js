8f9e5935ff81aaadba04fa05c067f599fd812fb2
muhdfarseen <muhdfarseen@gmail.com>
2024-10-17 17:15:31 +0530

    feat: Added refresh JWT Logic

--- src/config/api-end-points.js
+++ src/config/api-end-points.js
@@ -1,12 +1,10 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-
 const ENDPOINTS = {
     BASE_URL: BASE_URL,
 
     login: '/login/',
-    refresh : '/token/refresh/'
+    refresh: '/token/refresh/'
 };
-
 
 export { ENDPOINTS };
